92d65b302ac4b05765e425793ab2d7b3ccd3b709
Jaafar Rammal <jaafarrammal@sableur-mac.home>
2020-01-21 09:23:15 +0000

    Raspberry pi code

--- raspberrypi_code/rp bin code.py
+++ raspberrypi_code/rp bin code.py
@@ -3,9 +3,8 @@
 import RPi.GPIO as GPIO
 import paho.mqtt.client as mqtt
 import time
-
-client = mqtt.Client()
-client.connect("172.20.10.11",1234,60)
+import base64
+from subprocess import call
 
 GPIO.setmode(GPIO.BCM)
 TRIG = 23
@@ -14,11 +13,14 @@ GPIO.setup(TRIG,GPIO.OUT)
 GPIO.setup(ECHO,GPIO.IN)
 GPIO.output(TRIG, False)
 
+client = mqtt.Client()
+client.connect("172.20.10.11", 1234, 60)
+
 for n in range(30):
 
         print "Waiting For Sensor To Settle"
 
-        time.sleep(2)
+        time.sleep(15)
         GPIO.output(TRIG, True)
         time.sleep(0.00001)
         GPIO.output(TRIG, False)
@@ -32,6 +34,21 @@ for n in range(30):
         distance = pulse_duration*17150
         distance = round(distance, 2)
         print "Distance:",distance,"cm"
-        client.publish("topic/test", distance)
+        full = 100
+        if distance > 115:
+                full = 0
+        elif distance < 25:
+                full = 100
+        else:
+                full = 120 - distance
+		full = full * 0.8
+        message = "{\"id\":5,\"latitude\":50,\"longtitude\":0,\"full\":"+str(full)+"}"
+        client.publish("bin", message)
 
+	call(["fswebcam",  "./image.jpg"])
+	with open("image.jpg", "rb") as image:
+		b64string = base64.b64encode(image.read())
+		client.publish("image", b64string)
+
+client.disconnect()
 GPIO.cleanup()
